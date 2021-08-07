023365d69bb37bb6cce322d33121592a8280d762
Mevlüt Can Tuna <mttuna90@gmail.com>
2021-08-07 10:34:36 +0300

    procfile is added

--- server/index.js
+++ server/index.js
@@ -5,12 +5,12 @@ const cors = require("cors");
 const bodyParser = require('body-parser')
 const todoRouter = require("./routes/todos.js");
 
-dotenv.config()
 const app = express();
+dotenv.config()
 
-app.use(cors())
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 app.get("/",(req,res) => {
     res.send("Hello Everyone")
@@ -18,10 +18,11 @@ app.get("/",(req,res) => {
 
 app.use("/todos",todoRouter)
 
-mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true,useUnifiedTopology: true })
+mongoose
+    .connect(process.env.DB_URL,{ useNewUrlParser: true,useUnifiedTopology: true })
     .then(() => {
-        app.listen(process.env.PORT || 5000,() => {
-            console.log(`Working on ${process.env.PORT || 5000} PORT...` )
+        app.listen(process.env.PORT,() => {
+            console.log(`Working on ${process.env.PORT} PORT...` )
         })
     })
     .catch(err => console.log(err));
